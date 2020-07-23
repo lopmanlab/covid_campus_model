f10ca6b53ddae1b8c72f22be3db44ff4f8c95343
Carol <ykliu2680@gmail.com>
2020-07-22 22:57:53 -0400

    Update 1_screen_explore_plot.R

--- 1_screen_explore_plot.R
+++ 1_screen_explore_plot.R
@@ -35,21 +35,27 @@ df$scrInt <- rep(screen.int, each = 116)
 df <- select(df, c(-run, -time))
 dfLast <- filter(df, date == "2020-12-20")
 
-tiff("Plots/1_screen_explore.tiff", units="in", width=6, height=5, res=300)
+png("Plots/1_screen_explore.png", units="in", width=6, height=5, res=500)
 m <- rbind(c(1,2,5), c(3,4,5))
 print(m)
 layout(m)
 par(mar = c(3,3,2,1), mgp = c(2,1,0))
 plot(mod, y = "I_stu", main = "Active cases", col = pal, legend = FALSE,
      ylab = "Student cases", xlab = "")
+
 plot(mod, y = "Icum_stu", main = "Cumulative cases", col = pal, legend = FALSE,
      ylab = "", xlab = "")
+
+plot(mod, y = "Isym_saf", col = pal, legend = FALSE,
+     ylab = "Staff/Faculty cases", xlab = "Time (Days)",
+     ylim = c(0, max(mod$epi$I_stu)))
+
+plot(mod, y = "Icum_saf", col = pal, legend = FALSE,
+     ylab = "", xlab = "Time (Days)",
+     ylim = c(0, max(mod$epi$Icum_stu)))
+
 legend("topleft", legend = c("Weekly Screening", "Monthly Screening", "One-Time Screening"), lwd = 3,
        col = c(pal[1], pal[4], pal[length(pal)]), bty = "n", cex = 0.8)
-plot(mod, y = "Isym_saf", col = pal, legend = FALSE,
-     ylab = "Staff/Faculty cases", xlab = "Time (Days)")
-plot(mod, y = "Icum_saf", col = pal, legend = FALSE,
-     ylab = "", xlab = "Time (Days)")
 plot(dfLast$scrInt, dfLast$Icum_stu, type = "l", lwd = 5, col = pal[1], ylim = c(0,max(dfLast$Icum_stu)),
      ylab = "Cumulative Cases", xlab = "Screening Interval (Days)",
      main = "Cumulative Cases")
@@ -57,7 +63,7 @@ lines(dfLast$scrInt, dfLast$Icum_saf, lwd = 5, col = pal[length(pal)])
 legend("topleft", legend = c("Students", "Staff/Faculty"), lwd = 3,
        col = c(pal[1], pal[length(pal)]), bty = "n", cex = 0.8)
 
-#dev.off()
+dev.off()
 
 ## Reductions in cases from base case
 param <- param.dcm(latent = latent,
