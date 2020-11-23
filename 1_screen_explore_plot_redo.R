0cbb9d2ceeef0ed4d5696317ebb0f2bc8a88e554
Carol <ykliu2680@gmail.com>
2020-11-23 02:46:42 -0500

    Update 1_screen_explore_plot_redo.R

--- 1_screen_explore_plot_redo.R
+++ 1_screen_explore_plot_redo.R
@@ -17,38 +17,44 @@ options(digits=4)
 ## Loop through screening scenarios
 screen.int <- 1/seq(7,120,7)
 screen.int.days <- seq(7,120,7)
-pal <- brewer_ramp(length(screen.int), "Spectral")
-
 
 screen_list<-list()                   #Initialize list to collect results from each screening interval
-# Below loop runs model and then extracts median of active and cumulative student cases and active and cumulative staff cases
-
+# Below loop runs model for each screening scenario
 for (i in 1:length(screen.int)) {
-  screen_list[[i]]<-model_scenarios(screening=screen.int[i],screening_on=screen.int[i]) %>%
-                            mutate(scenario = rep(screen.int.days[i]))
+  screen_list[[i]]<-model_scenarios(screening=screen.int[i],screening_on=screen.int[i])
   
 }
 
-screen_df <- bind_rows(screen_list, .id = "column_label")
+#Below loop takes each scenario and computes median active and cumulative cases for students and staff on each day
+screen_list_cases <- list()
+for (i in 1:length(screen_list)){
+  screen_list_cases[[i]] <- getcases(screen_list[[i]]) %>%
+    mutate(scenario = rep(screen.int.days[i]))
+}
+
+screen_df <- bind_rows(screen_list_cases, .id = "column_label")
 # plot
+#Theme
 theme <- theme_classic()+ theme(legend.position = "none",
                                 plot.title = element_text(size=11),
                                 axis.text = element_text(size=9),
                                 axis.title = element_text(size=9))
+#Palette
+pal <- brewer_ramp(length(screen.int), "Spectral")
 
 
 p1 <- ggplot(data = screen_df, aes(x=time, y=med_stud_active))+geom_line(aes(colour=factor(scenario)),size=0.8) + 
-  scale_color_manual(values=pal) +xlab("") +ylab("Student cases")+ ggtitle("Active cases") + theme + ylim(0,350)
+  scale_color_manual(values=pal) +xlab("") +ylab("Student cases")+ ggtitle("Active cases") + theme + ylim(0,300)
 p2<-ggplot(data = screen_df, aes(x=time, y=med_stud_cum)) + geom_line(aes(colour=factor(scenario)),size=0.8)+
-  scale_color_manual(values=pal) +xlab("") +ylab("") + ggtitle("Cumulative cases") + theme+ ylim(0,3000)
+  scale_color_manual(values=pal) +xlab("") +ylab("") + ggtitle("Cumulative cases") + theme+ ylim(0,2500)
 p3<-ggplot(data = screen_df, aes(x=time, y=med_saf_active)) + geom_line(aes(colour=factor(scenario)),size=0.8)+
-  scale_color_manual(values=pal)+ylab("Staff/faculty cases")+xlab("Time (Days)") +theme + ylim(0,350)
+  scale_color_manual(values=pal)+ylab("Staff/faculty cases")+xlab("Time (Days)") +theme + ylim(0,300)
 
 p4<-ggplot(data = screen_df, aes(x=time, y=med_saf_cum)) + geom_line(aes(colour=factor(scenario)),size=0.8)+
   scale_color_manual(breaks=c("7","28","119"),
                      labels=c("Weekly Screen","Monthly Screen","One-time Screen"), 
                      values=pal)+
-  xlab("Time (Days)")+ylab("") + ylim(0,3000)+
+  xlab("Time (Days)")+ylab("") + ylim(0,2500)+
   theme+theme(legend.position = c(0.5, 0.85),legend.title = element_blank(), legend.text=element_text(size=8),legend.key.size = unit(0.3, "cm")) 
 
 dfLast <- filter(screen_df, time==116)%>%
@@ -58,9 +64,10 @@ dfLast <- filter(screen_df, time==116)%>%
 p5 <- ggplot(data=dfLast, aes(x=scenario, y=value)) + geom_line(aes(colour=factor(variable)),size=0.8) +
   scale_color_manual(labels= c("Student cases","Staff cases"),values=c(pal[1],pal[length(pal)])) +theme + 
   xlab("Screen Interval (Days)") +ylab("Cumulative Cases") + ggtitle("Cumulative cases")+ xlim(0,120)+
-  theme(legend.position = c(0.7, 0.1),legend.title = element_blank(), legend.text=element_text(size=8),legend.key.size = unit(0.3, "cm"))
+  scale_x_continuous(name = "Screening interval (Days)",breaks = c(0,30,60,90,120), labels =c(0,30,60,90,120))+
+  theme(legend.position = c(0.7, 0.08),legend.title = element_blank(), legend.text=element_text(size=8),legend.key.size = unit(0.3, "cm"))
 
-png("Plots/1_screen_explore_redo_psa_new.png", units="in", width=6, height=5, res=500)
+png("Plots/11222020/fig2_screen_explore_redo_psa_new.png", units="in", width=6, height=5, res=800)
 grid.arrange(
   p1,p2,p3,p4,p5,
   widths = c(2, 2, 2),
@@ -69,7 +76,8 @@ grid.arrange(
 )
 dev.off()
 
-saveRDS(screen_df,"tables/res_fig1.RDS")
+saveRDS(screen_df,"Plots/11222020/res_fig2.RDS")
+saveRDS(screen_list,"Plots/11222020/res_fig2_rawmodeloutputs.RDS")
 
 
 
