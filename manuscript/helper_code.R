dbbb15cd4493577d9af1c06f7ad93320a8fef9a9
Carol <ykliu2680@gmail.com>
2020-12-07 21:45:11 -0500

    Delete helper_code.R
    re arrange folders

--- manuscript/helper_code.R
+++
@@ -1,9 +0,0 @@
-library("docxtractr")
-library("here")
-
-worddoc <- docxtractr::read_docx("EmoryCampusModel_Preprint_v2.docx")
-
-tab1 = docxtractr::docx_extract_tbl(worddoc, tbl_number = 3)
-
-tablefile = paste0(here('tables'),'/paramtable.Rds')
-saveRDS(tab1, file = tablefile)
